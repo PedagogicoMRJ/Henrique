c363bd46c3a9a8d6dc32dc41d9f3ac5ca886dbdb
Prof <pedagogicomyrobotjoinville@gmail.com>
2025-10-18 12:05:12 -0300

    continuação aula 8

--- MY 2D plataform/Assets/Scripts/Robot.cs
+++ MY 2D plataform/Assets/Scripts/Robot.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
+    // Verifica que armazena o estado de pulo do robô
+    public bool isjumping;
     // Variável que armazena o valor da velocidade do robô
     public float speed;
     // Variável que armazena o valor da força do pulo do robô
     public float jump;
     // Variável que recebe o componente de física 2D rigidbody
-    private Ridigbody2D rig;
+    private Rigidbody2D rig;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +23,12 @@ public class Robot : MonoBehaviour
     void Update()
     {
         // Chamaa função Movement
-        Movement;
+        Movement();
         // Chama a função Jump
         Jump();
     }
     // Função que controla o movimento do robô
-    void Moviment()
+    void Movement()
     {
         // Vetor que recebe o pararetro que define a direção do movimento do robô
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
@@ -36,13 +38,39 @@ public class Robot : MonoBehaviour
     // Função que controla o pulo do robô
     void Jump()
     {
-        // Verifica que o botão tag "Jump" foi pressionado
-        if (Input.GetButton("Jump"))
-        {
-            // Adiciona impulso ao rigidbody 2D do robô de acordo com o valor de jump
-            rig.AddForce(new Vector2(0f, jump ),)
-        }
+        // Verifica que o botão com a tag "Jump" foi pressionado
+        if (Input.GetButtonDown("Jump") && !isjumping)
+            // Verifica que o botão tag "Jump" foi pressionado
+            if (Input.GetButtonDown("Jump"))
+            {
+                // Adiciona impulso ao rigidbody 2D do robô de acordo com o valor de jump
+                rig.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+            }
     }
 
-}
+    // Função ativada ao ocorrer uma colisão
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Verifica se o objeto colidido possui a layer 6
+        if (collision.gameObject.layer == 6)
+        {
+            // Atribui o valor falso a variavel isjumping
+            isjumping = false;
+        } 
+        }
+        // Função ativa quando para de ocorrer uma colisão
+        private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Verifica se o objeto colidido possuí a layer 6
+        if(collision.gameObject.layer == 6)
+        {
+            // Atribui o valor verdadeiro a variavel isjumping
+            isjumping = true;   
+        }
+    }
+    }
+    
+    
+
+
 
